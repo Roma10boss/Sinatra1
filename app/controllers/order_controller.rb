7ca3ae51619e58da7a4b021360ab47dcf138382b
Romario Gustave <romagustave10@gmail.com>
2020-08-04 18:49:07 +0000

    Error when submit

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,21 @@
+class OrderController < ApplicationController
+  #Create
+  get '/orders/new' do
+    erb :'/orders/new'
+  end
+
+  post '/orders' do
+    @order = Order.create(
+    botlle: params[:bottle],
+    ounces: params[:ounces]
+    )
+    redirect "/orders/#{@order.id}"
+  end
+
+  #read
+  get '/orders/:id' do
+    @order = Order.find(params[:id])
+    erb :'/orders/show'
+  end
+
+end
